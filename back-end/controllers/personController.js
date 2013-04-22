abc02e000ed3d5681916bda4f40a318c3fa56f40
linaro <linaro@linaro-ubuntu-desktop.(none)>
2013-04-22 18:58:02 +0000

    name of mysql database to be used is homepagedb instead of homepageDB

--- back-end/controllers/personController.js
+++ back-end/controllers/personController.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
-    database: "homepageDB"
+    database: "homepagedb"
 });
 
 exports.getAllPersons = function(req, res){
